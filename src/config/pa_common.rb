38072d8a5b7bcc929cac3edcfea4736b1399fbaf
Cameron Palk <ckpalk@comcast.net>
2017-02-28 09:38:40 -0800

    Initial commit with chrome extension buildable and identifying videos

---
+++ src/config/pa_common.rb
@@ -0,0 +1,5 @@
+@PRODUCT_TITLE = "Project Awesome Video Ad Controller"
+@PRODUCT_DESCRIPTION = "This extension will help you to curate your video ad choices"
+#Image folder for asserts
+@IMG_FOLDER = "img_pa"
+@VERSION = "1.0.0"
---
+++ src/config/project_awesome.rb
@@ -0,0 +1,11 @@
+#Debug flag is for futurre use
+@DEBUG_FLAG = "false"
+
+@base_url = "https://prefs.projectawesome.xyz"
+@awesome_prefcenter = "prefs.projectawesome.xyz"
+@awesome_cookie_url = "http://prefs.projectawesome.xyz"
+@awesome_cookie_domain = "prefs.projectawesome.xyz"
+
+@post_inst_page_url = @base_url + "/plugin"
+
+@gaTrackingId = "UA-79735862-4"
